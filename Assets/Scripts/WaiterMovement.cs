bbd835e17a7e217685f744382bcf1e1cb7599e83
Renaud Marshall <renaud.marshall@gmail.com>
2018-07-13 19:57:41 -0400

    Added the waiter and chase mechanic
    Now you need to run

---
+++ Assets/Scripts/WaiterMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterMovement : MonoBehaviour {
+
+	public GameObject target;
+	public float speed = 4.0f;
+
+	private void Awake() {
+		target = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("You got caught");
+			target = null;
+			Destroy(other.gameObject);
+		}
+	}
+}
