bdf6934c866329c350d51d32bd8bc4d65158c6e4
Mylo Gonzalez <josejavgonzalez01@gmail.com>
2018-07-14 12:14:39 -0400

    Basic Player Clamping. Will need to offset

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 7.0f;
+    public BoxCollider2D AreaBounds;
+    private BoxCollider2D PlayerBounds;
     private string sceneToGoTo = "GameOver";
 
+    private void Start()
+    {
+        PlayerBounds = GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
+
         gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
 
         if (Input.GetKey(KeyCode.Escape))
@@ -18,6 +25,15 @@ public class PlayerMovement : MonoBehaviour
             sceneToGoTo = "MainMenu";
             Destroy(gameObject);
         }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(PlayerBounds.size.x, AreaBounds.bounds.min.x, AreaBounds.bounds.max.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, AreaBounds.bounds.min.y, AreaBounds.bounds.max.y);
+
+        transform.position = clampedPosition;
     }
 
     private void OnDestroy()
