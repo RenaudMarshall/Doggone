0542b15bc34a95c68ce1d33761018ce99f0e4587
Renaud Marshall <renaud.marshall@gmail.com>
2018-07-14 07:52:08 -0400

    Correcting a merge conflict from 553283c

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,22 +9,9 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 7.0f;
     private string sceneToGoTo = "GameOver";
 
-<<<<<<< HEAD
     void Update()
     {
         gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
-=======
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
-	}
->>>>>>> 7e6197653a4038b931a5784b4ab4470d92f84887
 
         if (Input.GetKey(KeyCode.Escape))
         {
