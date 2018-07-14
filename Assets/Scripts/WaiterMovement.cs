627c9872a89d61bc8a529332460cd95b40a00b62
Renaud Marshall <renaud.marshall@gmail.com>
2018-07-14 08:47:06 -0400

    Added a vision cone to make the waiter chase when the dog enters sight

--- Assets/Scripts/WaiterMovement.cs
+++ Assets/Scripts/WaiterMovement.cs
@@ -7,10 +7,13 @@ public class WaiterMovement : MonoBehaviour
 
     public GameObject target;
     public float speed = 4.0f;
+    BoxCollider2D bodyCollider;
+    PolygonCollider2D sightCollider;
 
     private void Awake()
     {
-        target = GameObject.FindGameObjectWithTag("Player");
+        //target = GameObject.FindGameObjectWithTag("Player");
+        //bodyCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/WaiterSight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterSight : MonoBehaviour
+{
+
+    WaiterMovement waiterMovement;
+
+    private void Awake()
+    {
+        waiterMovement = GetComponentInParent<WaiterMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("You were seen");
+            waiterMovement.target = other.gameObject;
+        }
+    }
+}
