8ea0576c444567c49cb35adc9ee6341a9ea43009
Mylo Gonzalez <josejavgonzalez01@gmail.com>
2018-07-14 17:30:34 -0400

    GameController created

--- Assets/Scripts/FoodTrigger.cs
+++ Assets/Scripts/FoodTrigger.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class FoodTrigger : MonoBehaviour
 {
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("got the thing");
+            gameController.AddPoints(10);
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private int TotalPoints;
+	// Use this for initialization
+	void Start () {
+        TotalPoints = 0;
+	}
+	
+	public void AddPoints(int add)
+    {
+        TotalPoints += add;
+        Debug.Log(TotalPoints);
+    }
+}
